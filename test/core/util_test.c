24362594fd0d7d4ae9a25f4b18ac4d1e20e67271
Sergey Agievich <agievich@gmail.com>
2019-06-11 19:39:30 +0200

    test/core/util += utilInfo

--- test/core/util_test.c
+++ test/core/util_test.c
@@ -5,7 +5,7 @@
 \project bee2/test
 \author (C) Sergey Agievich [agievich@{bsu.by|gmail.com}]
 \created 2017.01.17
-\version 2017.01.17
+\version 2019.06.11
 \license This program is released under the GNU General Public License 
 version 3. See Copyright Notices in bee2/info.h.
 *******************************************************************************
@@ -13,6 +13,27 @@ version 3. See Copyright Notices in bee2/info.h.
 
 #include <stdio.h>
 #include <bee2/core/util.h>
+
+/*
+*******************************************************************************
+Информация о runtime-среде и настройках
+*******************************************************************************
+*/
+
+const char* utilInfo()
+{
+	static char descr[128];
+	sprintf(descr, "%s,B_PER_W=%d,B_PER_S=%d,%s",
+		(OCTET_ORDER == LITTLE_ENDIAN) ? "LITTLE_ENDIAN" : "BIG_ENDIAN",
+		B_PER_W, B_PER_S,
+#ifdef SAFE_FAST
+		"FAST"
+#else
+		"SAFE"
+#endif
+		);
+	return descr;
+}
 
 /*
 *******************************************************************************
@@ -24,7 +45,7 @@ version 3. See Copyright Notices in bee2/info.h.
 
 bool_t utilTest()
 {
-	printf("utilVersion: %s\n", utilVersion());
+	printf("utilVersion: %s [%s]\n", utilVersion(), utilInfo());
 	if (utilMin(5, SIZE_1, (size_t)2, (size_t)3, SIZE_1, SIZE_0) != 0 ||
 		utilMax(5, SIZE_1, (size_t)2, (size_t)3, SIZE_1, SIZE_0) != 3)
 		return FALSE;
