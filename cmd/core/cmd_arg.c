db4d43b057466456e002b8349b07ee826f8eb061
Sergey Agievich <agievich@gmail.com>
2025-06-10 14:42:20 +0300

    Make cmdArgCreate() platform independent

--- cmd/core/cmd_arg.c
+++ cmd/core/cmd_arg.c
@@ -4,7 +4,7 @@
 \brief Command-line interface to Bee2: parsing arguments
 \project bee2/cmd 
 \created 2022.06.08
-\version 2025.06.09
+\version 2025.06.10
 \copyright The Bee2 authors
 \license Licensed under the Apache License, Version 2.0 (see LICENSE.txt).
 *******************************************************************************
@@ -23,130 +23,128 @@
 *******************************************************************************
 */
 
-#ifdef OS_UNIX
+static size_t argWsDec(const char* args)
+{
+	size_t c = 0;
+	while (args[c] == ' ' || args[c] == '\t')
+		++c;
+	return c;
+}
 
-#include <wordexp.h>
+static size_t argArgDec(char* arg, size_t* size, const char* args)
+{
+	size_t c;
+	size_t s;
+	bool_t quotes;	
+	// pre
+	ASSERT(strIsValid(args));
+	ASSERT(memIsNullOrValid(size, O_PER_S));
+	ASSERT(argWsDec(args) == 0);
+	// просмотреть символы args
+	for (c = s = 0, quotes = FALSE; args[c] != '\0';)
+	{
+		// разделитель?
+		if (!quotes && (args[c] == ' ' || args[c] == '\t'))
+			break;
+		// кавычка?
+		if (args[c] == '"')
+		{
+			// открывающая?
+			if (!quotes)
+				++c, quotes = TRUE;
+			// двойная?
+			else if (args[c + 1] == '"')	
+			{
+				if (arg)
+					arg[s] = '"';
+				++s, c += 2;
+			}
+			// закрывающая?
+			else
+				++c, quotes = FALSE;
+			continue;
+		}
+		// обратный слэш?
+		if (args[c] == '\\')
+		{
+			size_t c1;
+			// c1 <- число слэшей
+			for (c1 = 1; args[c + c1] == '\\'; ++c1);
+			// кавычка после c1 слэшей?
+			if (args[c + c1] == '"')
+			{
+				if (arg)
+					memCopy(arg + s, args + c, c1 / 2);
+				s += c1 / 2, c += c1;
+				// кавычка-литерал?
+				if (c1 % 2)
+				{
+					if (arg)
+						arg[s] = '"';
+					++s, ++c;
+				}
+			}
+			// без кавычки
+			else
+			{
+				if (arg)
+					memCopy(arg + s, args + c, c1);
+				s += c1, c += c1;
+			}	
+			continue;
+		}
+		// регулярный символ
+		if (arg)
+			arg[s] = args[c];
+		++s, ++c;
+	}
+	// незакрытая кавычка? значит строка args исчерпана
+	ASSERT(!quotes || args[c] == '\0');
+	// завершить
+	if (arg)
+		arg[s] = '\0';
+	if (size)
+		*size = s + 1;
+	return c + argWsDec(args + c);
+}
 
 err_t cmdArgCreate(int* argc, char*** argv, const char* args)
 {
-	wordexp_t we[1];
 	size_t count;
+	size_t size;
 	int pos;
+	void* state;
+	char* strs;
 	// входной контроль
 	ASSERT(strIsValid(args));
 	ASSERT(memIsDisjoint2(argc, sizeof(int), argv, sizeof(char**)));
-	// разбить args на строки широких символов
-	switch (wordexp(args, we, 0))
+	// определить число и длины аргументов
+	for (*argc = 0, size = 0, count = argWsDec(args); args[count] != '\0';)
 	{
-	case 0:
-		break;
-	case WRDE_NOSPACE:
+		size_t c;
+		size_t s;
+		c = argArgDec(0, &s, args + count);
+		*argc += 1, count += c, size += s;
+	}
+	// выделить и разметить память
+	if (!(state = blobCreate(*argc * sizeof(char*) + size)))
 		return ERR_OUTOFMEMORY;
-	default:
-		return ERR_CMD_PARAMS;
-	}
-	*argv = 0, *argc = we->we_wordc;
-	// обработать отсутствие аргументов
-	if (*argc == 0)
+	*argv = (char**)state;
+	strs = (char*)(*argv + *argc);
+	// разобрать аргументы
+	for (size = 0, pos = 0, count = argWsDec(args); args[count] != '\0';)
 	{
-		wordfree(we);
-		return ERR_OK;
+		size_t c;
+		size_t s;
+		(*argv)[pos] = strs + size;
+		c = argArgDec((*argv)[pos], &s, args + count);
+		count += c, size += s, ++pos;
 	}
-	// перенести аргументы в блоб
-	count = (size_t)(*argc) * sizeof(char*);
-	for (pos = 0; pos < *argc; ++pos)
-		count += strLen(we->we_wordv[pos]) + 1;
-	if (!(*argv = blobCreate(count)))
-	{
-		wordfree(we);
-		return ERR_OUTOFMEMORY;
-	}
-	count = (size_t)(*argc) * sizeof(char*);
-	for (pos = 0; pos < *argc; ++pos)
-	{
-		(*argv)[pos] = (char*)*argv + count;
-		strCopy((*argv)[pos], we->we_wordv[pos]);
-		count += strLen((*argv)[pos]) + 1;
-	}
-	// завершить
-	wordfree(we);
+	ASSERT(pos == *argc);
 	return ERR_OK;
 }
-
-#elif defined OS_WIN
-
-#include <windows.h>
-
-err_t cmdArgCreate(int* argc, char*** argv, const char* args)
-{
-	wchar_t* argsw = 0;
-	wchar_t** argvw = 0;
-	size_t count;
-	int pos;
-	// pre
-	ASSERT(strIsValid(args));
-	ASSERT(memIsDisjoint2(argc, sizeof(int), argv, sizeof(char**)));
-	// разбить args на строки широких символов
-	if (!(argsw = memAlloc((strLen(args) + 1) * sizeof(wchar_t))))
-		return ERR_OUTOFMEMORY;
-	if (!MultiByteToWideChar(CP_ACP, 0, args, -1, argsw, (int)strLen(args) + 1) ||
-		!(argvw = CommandLineToArgvW(argsw, argc)))
-	{
-		memFree(argsw);
-		return ERR_CMD_PARAMS;
-	}
-	memFree(argsw);
-	ASSERT(*argc >= 0);
-	// обработать отсутствие аргументов
-	*argv = 0;
-	if (*argc == 0)
-		return ERR_OK;
-	// перенести аргументы в блоб
-	count = (size_t)(*argc) * sizeof(char*);
-	for (pos = 0; pos < *argc; ++pos)
-	{
-		int len = WideCharToMultiByte(CP_ACP, 0, argvw[pos], -1,
-			NULL, 0, NULL, NULL);
-		if (!len)
-		{
-			LocalFree(argvw);
-			return ERR_CMD_PARAMS;
-		}
-		count += (size_t)len;
-	}
-	if (!(*argv = blobCreate(count)))
-	{
-		LocalFree(argvw);
-		return ERR_OUTOFMEMORY;
-	}
-	count = (size_t)(*argc) * sizeof(char*);
-	for (pos = 0; pos < *argc; ++pos)
-	{
-		int len;
-		(*argv)[pos] = (char*)*argv + count;
-		len = WideCharToMultiByte(CP_ACP, 0, argvw[pos], -1, NULL, 0,
-			NULL, NULL);
-		ASSERT(len);
-		WideCharToMultiByte(CP_ACP, 0, argvw[pos], -1, (*argv)[pos], len,
-			NULL, NULL);
-		count += (size_t)len;
-	}
-	// завершить
-	LocalFree(argvw);
-	return ERR_OK;
-}
-
-#else
-
-err_t cmdArgCreate(int* argc, char*** argv, const char* args)
-{
-	return ERR_NOT_IMPLEMENTED;
-}
-
-#endif
 
 void cmdArgClose(char** argv)
 {
 	blobClose(argv);
 }
-
