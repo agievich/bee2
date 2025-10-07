395bdcad94f59b7d9c12f1fede0d9a8b3ccd0afe
Sergey Agievich <agievich@gmail.com>
2025-10-07 19:50:49 +0300

    Add synthetic timer-counter to core/rng/timer

--- src/core/rng.c
+++ src/core/rng.c
@@ -4,7 +4,7 @@
 \brief Entropy sources and random number generators
 \project bee2 [cryptographic library]
 \created 2014.10.13
-\version 2025.10.03
+\version 2025.10.07
 \copyright The Bee2 authors
 \license Licensed under the Apache License, Version 2.0 (see LICENSE.txt).
 *******************************************************************************
@@ -202,7 +202,7 @@ err:
 }
 
 #elif (defined(__GNUC__) || defined(__clang__)) && \
-  (defined(__i386__) || defined(__x86_64__))
+	(defined(__i386__) || defined(__x86_64__))
 
 #include <cpuid.h>
 
@@ -523,6 +523,150 @@ static err_t rngSys2Read(void* buf, size_t* read, size_t count)
 
 /*
 *******************************************************************************
+Таймер-счетчик
+
+Таймер-счетчик может использоваться вместо стандартного таймера, если частота
+обновления последнего недостаточно высока. Таймер-счетчик -- это отдельный 
+поток, в котором выполняется исключительно инкремент счетчика. Частота 
+обновления счетчика проверяется при создании таймера. Таймер не создается,
+если частота обновления ниже 10^8 раз в секунду (100 МГц).
+
+\remark Синхронизация не нужна, поскольку функции таймера-счетчика вызываются
+только в сихнронизированных участках кода.
+
+\thanks Подсмотрено в https://github.com/smuellerDD/jitterentropy-library.
+*******************************************************************************
+*/
+
+static volatile bool_t _tm_ctr_loop;		/*< счетчик запущен? */
+static volatile tm_ticks_t _tm_ctr_ticks;	/*< текущее показание */
+
+#ifdef OS_WIN
+
+static DWORD tmCtrLoop(void* args)
+{
+	while (_tm_ctr_loop)
+		_tm_ctr_ticks++;
+	return 0;
+}
+
+static HANDLE _tm_ctr_tid;
+
+static bool_t tmCtrCreate()
+{
+	if (_tm_ctr_loop)
+		return TRUE;
+	_tm_ctr_loop = TRUE;
+	_tm_ctr_tid = CreateThread(0, 0, tmCtrLoop, 0, 0, 0);
+	if (!_tm_ctr_tid)
+		return _tm_ctr_loop = FALSE;
+	return TRUE;
+}
+
+static void tmCtrClose()
+{
+	if (_tm_ctr_loop)
+	{
+		_tm_ctr_loop = FALSE;
+		(void)WaitForSingleObject(_tm_ctr_tid, INFINITE);
+		(void)CloseHandle(_tm_ctr_tid);
+		_tm_ctr_ticks = 0;
+		_tm_ctr_tid = 0;
+	}		
+}
+
+#elif defined OS_UNIX
+
+static void* tmCtrLoop(void* args)
+{
+	while (_tm_ctr_loop)
+		 _tm_ctr_ticks++;
+	return 0;
+}
+
+pthread_t _tm_ctr_tid;
+
+static bool_t tmCtrCreate()
+{
+	if (_tm_ctr_loop)
+		return TRUE;
+	_tm_ctr_loop = TRUE;
+	if (pthread_create(&_tm_ctr_tid, 0, tmCtrLoop, 0) != 0)
+	if (!_tm_ctr_tid)
+		return _tm_ctr_loop = FALSE;
+	return TRUE;
+}
+
+static void tmCtrClose()
+{
+	if (_tm_ctr_loop)
+	{
+		_tm_ctr_loop = FALSE;
+		(void)pthread_join(_tm_ctr_tid, 0);
+		_tm_ctr_ticks = 0;
+		_tm_ctr_tid = 0;
+	}		
+}
+
+#else
+
+static bool_t tmCtrCreate()
+{
+	return FALSE;
+}
+
+static void tmCtrClose()
+{
+}
+
+#endif
+
+static bool_t tmCtrIsValid()
+{
+	return _tm_ctr_loop;
+}
+
+static tm_ticks_t tmCtrTicks()
+{
+	return _tm_ctr_ticks;
+}
+
+static bool_t tmCtrStart()
+{
+	tm_ticks_t start;
+	tm_ticks_t overhead;
+	tm_ticks_t freq;
+	// таймер уже создан?
+	if (tmCtrIsValid())
+		return TRUE;
+	// создать
+	if (!tmCtrCreate())
+		return FALSE;
+	// оценить частоту
+	start = tmCtrTicks();
+	overhead = tmCtrTicks() - start;
+	start = tmCtrTicks();
+	mtSleep(100);
+	freq = tmCtrTicks();
+	freq = (freq - start - overhead) * 10;
+	// проверить частоту
+	if (freq < 100000000u)
+	{
+		tmCtrClose();
+		return FALSE;
+	}
+	// зарегистрировать закрытие
+	if (!utilOnExit(tmCtrClose))
+	{
+		tmCtrClose();
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+/*
+*******************************************************************************
 Источник-таймер
 
 Реализовано предложение [Jessie Walker, Seeding Random Number Generator]:
@@ -536,6 +680,11 @@ static err_t rngSys2Read(void* buf, size_t* read, size_t count)
 Реализация:
 -	таймер может быть источником случайности, если он обновляется не реже
 	10^9 раз в секунду (1 ГГц);
+-	если частота обновления таймера ниже разрешенной, то вместо него может 
+	использоваться таймер-счетчик с частотой обновления не ниже 10^8 раз 
+	в секунду (100 MГц). Снижение порога частоты мотивируется тем, что 
+	таймер-счетчик по принципу своей работы привносит случайные флуктуации 
+	промежутков времени между отсчетами; 
 -	для формирования одного выходного бита используется сумма битов четности
 	8-ми разностей между показаниями таймера.
 
@@ -555,7 +704,8 @@ static err_t rngSys2Read(void* buf, size_t* read, size_t count)
 
 static bool_t rngTimerIsAvail()
 {
-	return tmFreq() >= 1000000000u;
+	return //tmFreq() >= 1000000000u || 
+		tmCtrStart();
 }
 
 static err_t rngTimerRead(void* buf, size_t* read, size_t count)
@@ -581,7 +731,7 @@ static err_t rngTimerRead(void* buf, size_t* read, size_t count)
 			for (reps = 0; reps < 8; ++reps)
 			{
 				mtSleep(0);
-				t = tmTicks();
+				t = tmCtrIsValid() ? tmCtrTicks() : tmTicks();
 				w ^= (word)(t - ticks);
 				ticks = t;
 			}
